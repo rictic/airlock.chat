8b88419208111c0d3907b541e08d5b4bb9303d55
Peter Burns <rictic@gmail.com>
2020-09-25 15:31:09 -0700

    Draw a smiley face

--- www/index.js
+++ www/index.js
@@ -3,3 +3,8 @@ import * as wasm from "rust-us";
 const output = document.createElement('div');
 output.innerText = wasm.greet();
 document.body.appendChild(output);
+
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+document.body.appendChild(canvas);
+wasm.draw();

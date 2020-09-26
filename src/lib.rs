6258b391440939481747f2723bfcf5a0c47ead4d
Peter Burns <rictic@gmail.com>
2020-09-25 17:04:34 -0700

    Arrow keys control left eye.

--- src/lib.rs
+++ src/lib.rs
@@ -9,51 +9,79 @@ use wasm_bindgen::JsCast;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// These params describe whether the player is currently holding down each
-// of the buttons.
 #[wasm_bindgen]
-pub fn draw(up: bool, _down: bool, _left: bool, _right: bool) -> Option<bool> {
-    let document = web_sys::window().unwrap().document()?;
-    let canvas = document.get_element_by_id("canvas")?;
-    let canvas: web_sys::HtmlCanvasElement =
-        canvas.dyn_into::<web_sys::HtmlCanvasElement>().ok()?;
+pub struct Game {
+    x: f64,
+    y: f64,
+}
 
-    let context = canvas
-        .get_context("2d")
-        .ok()??
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .ok()?;
+#[wasm_bindgen]
+impl Game {
+    // These params describe whether the player is currently holding down each
+    // of the buttons.
+    pub fn draw(&mut self, up: bool, down: bool, left: bool, right: bool) -> Option<bool> {
+        let document = web_sys::window().unwrap().document()?;
+        let canvas = document.get_element_by_id("canvas")?;
+        let canvas: web_sys::HtmlCanvasElement =
+            canvas.dyn_into::<web_sys::HtmlCanvasElement>().ok()?;
 
-    context.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
+        let context = canvas
+            .get_context("2d")
+            .ok()??
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .ok()?;
 
-    if !up {
-        return Some(true);
+        context.clear_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
+
+        if up {
+            self.y -= 1.0
+        }
+        if down {
+            self.y += 1.0
+        }
+        if left {
+            self.x -= 1.0
+        }
+        if right {
+            self.x += 1.0
+        }
+
+        context.begin_path();
+
+        // Draw the outer circle.
+        context
+            .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+            .ok()?;
+
+        // Draw the mouth.
+        context.move_to(110.0, 75.0);
+        context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).ok()?;
+
+        // Draw the left eye.
+        context.move_to(65.0 + self.x, 65.0 + self.y);
+        context
+            .arc(
+                60.0 + self.x,
+                65.0 + self.y,
+                5.0,
+                0.0,
+                f64::consts::PI * 2.0,
+            )
+            .ok()?;
+
+        // Draw the right eye.
+        context.move_to(95.0, 65.0);
+        context
+            .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+            .ok()?;
+
+        context.stroke();
+
+        Some(true)
     }
+}
 
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .ok()?;
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).ok()?;
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .ok()?;
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .ok()?;
-
-    context.stroke();
-
-    Some(true)
+#[wasm_bindgen]
+pub fn make_game() -> Game {
+    Game { x: 0.0, y: 0.0 }
 }

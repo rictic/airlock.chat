56e78297d978392a8ad3fa649d9e08d95c3e6fb1
Peter Burns <rictic@gmail.com>
2020-09-27 13:48:54 -0700

    Add a simple websocket rebroadcast server.
    Taken from https://github.com/snapview/tokio-tungstenite/blob/master/examples/server.rs this server takes each incoming message and rebroadcasts it to every other client.

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ else
   (cd www && npm ci)
 fi
 
-(wasm-pack build && cd www && npm install && npm run start) &
+(wasm-pack build && cd www && npm run start) &
 cargo install cargo-watch
+(cd server && cargo watch -x run) &
 cargo watch -s 'wasm-pack build'

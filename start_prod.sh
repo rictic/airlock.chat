c5a3572e61fda848ca21c83ee1c366bb87ea735e
Peter Burns <rictic@gmail.com>
2020-10-11 14:45:04 -0700

    Switch from raw hyper to warp.
    Warp is a friendlier abstraction on top of hyper that has lots of nice features, like built in static file serving lol

--- start_prod.sh
+++ start_prod.sh
@@ -8,7 +8,6 @@ echo "Building client..."
 (cd client && wasm-pack build --release && cd ../www && npm run build)
 rm -rf server/dist/
 cp -r www/dist server/dist
-gzip -9 server/dist/*
 echo "Client built, building the server..."
 cargo build --release
 
